da872373e54738cdbe2c06dbaf8c5c9cb09f9fb1
Ananya-Ananda <ananya.ananda201@gmail.com>
2022-05-31 17:15:35 -0400

    displayed all user info + link to individual user profile

--- frontend-spotify-project/src/Components/IndivProfilePage.js
+++ frontend-spotify-project/src/Components/IndivProfilePage.js
@@ -1,9 +1,15 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
+import { useLocation } from 'react-router-dom';
 
 function IndivProfilePage(props){
+    const location = useLocation();
+    const USERNAME = location.state?.id;
+    const [userInfo, setUserInfo] = useState(props.userInfo)
     return(
         <div>
-            Name here
+           Name Here
+           {console.log(USERNAME)}
+           {/* {userInfo.username} */}
         </div>
     );
 }export default IndivProfilePage;
